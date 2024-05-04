ab014341458653b377bb4cc76a44c021f148c7f5
Art Frich <frich.g23@yandex.ru>
2024-05-04 05:39:24 +0500

    hotfix/deploy f5 crash удалить мусор

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      '~': resolve(__dirname, 'src/'),
+export default () => {
+  dotenv.config({ path: `./.env` });
+  const mode = process.env.VITE_MODE;
+
+  return defineConfig({
+    plugins: [react()],
+    base: `${mode === 'dev' ? '/' : '/front/'}`,
+    server: {
+      host: true,
+      port: 3000,
     },
-  },
-});
+    resolve: {
+      alias: {
+        '~': resolve(__dirname, 'src/'),
+      },
+    },
+  });
+};
