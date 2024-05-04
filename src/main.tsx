a82da09a39f6247d98701ab272d8728020affdec
Art Frich <frich.g23@yandex.ru>
2024-05-04 05:54:44 +0500

    hotfix/deploy f5 crash

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Components/App/App.tsx';
 import { CssBaseline, StyledEngineProvider } from '@mui/material';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CssBaseline />
-    <BrowserRouter>
+    <HashRouter>
       <StyledEngineProvider injectFirst>
         <App />
       </StyledEngineProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
