979c7e973eeb019b969c9e83040121bed11728a0
Art Frich <frich.g23@yandex.ru>
2024-05-08 14:32:24 +0500

    hotfix/add check operator

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,8 +2,8 @@ import TelegramIcon from '@mui/icons-material/Telegram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import XIcon from '@mui/icons-material/X';
 
-export const BOT_URL = import.meta.env.VITE_BOT_URL.slice(1, -2) || 'https://t.me/Clicker_Game_Blockchain_Bot/game';
-export const BACK_URL = import.meta.env.VITE_BACK_URL.slice(1, -2) || 'https://9bee-176-109-111-182.ngrok-free.app';
+export const BOT_URL = import.meta.env.VITE_BOT_URL?.slice(1, -2) || 'https://t.me/Clicker_Game_Blockchain_Bot/game';
+export const BACK_URL = import.meta.env.VITE_BACK_URL?.slice(1, -2) || 'https://9bee-176-109-111-182.ngrok-free.app';
 // export const BOT_URL = 'https://t.me/byUlbiTVtestbot/something';
 // export const BACK_URL = 'http://localhost:5000';
 
